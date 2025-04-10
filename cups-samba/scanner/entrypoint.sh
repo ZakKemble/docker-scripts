e83a11edf90810d43f9385ff0677505d712d2196
Zak Kemble <3110616+ZakKemble@users.noreply.github.com>
2025-04-10 14:16:50 +0100

    add LABEL

--- cups-samba/scanner/entrypoint.sh
+++ cups-samba/scanner/entrypoint.sh
@@ -14,7 +14,7 @@ trap onSignal INT TERM
 
 beep -f 3000 -l 100 -D 50 -n -f 3000 -l 100 -d 50 -n -f 5000 -l 200
 
-useradd -M -u ${BRSCAN_USER:-2000} -g 0 -r -s /usr/sbin/nologin scanner 
+useradd -M -u ${BRSCAN_USER:-2000} -g 0 -r -s /usr/sbin/nologin scanner
 #runuser -u scanner brscan-skey
 brscan-skey
 
